48ed048de583146715a73a2834337dc4b1d63d1c
Noohani Owais <owais.noohani99@gmail.com>
2024-06-29 01:08:22 +0500

    Add .gitattributes for line ending conversion

---
+++ src/main/java/RetailDiscountCalculator/RetailDiscountCalculatorApplication.java
@@ -0,0 +1,13 @@
+package RetailDiscountCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailDiscountCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailDiscountCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RetailDiscountCalculator/RetailDiscountCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package RetailDiscountCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailDiscountCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
